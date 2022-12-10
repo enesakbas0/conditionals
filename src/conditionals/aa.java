c69db6f93d0f095b7680c95a6382bbe197b39196
Hasan <Hasan@10.44.104.22>
2022-12-10 16:06:23 +0300

    commit

---
+++ src/conditionals/aa.java
@@ -0,0 +1,19 @@
+package conditionals;
+
+public class aa {
+
+	public static void main(String[] args) {
+	
+		int sayi = 30 ;
+		if(sayi<40) {
+		System.out.println("sayi 40`tan küçüktür");
+		
+		}else if(sayi==40) {
+		System.out.println("sayi 40`a eşittir");
+		}else {
+		System.out.println("sayi 40`tan büyüktür");
+		}
+
+	}
+
+}
